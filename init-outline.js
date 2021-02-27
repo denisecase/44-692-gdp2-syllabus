2aeba22c217b5ef4b82eee0288589e671ab0569a
Denise Case <denisecase@users.noreply.github.com>
2021-02-27 12:16:19 -0600

    Ready to start development in mod 4

--- init-outline.js
+++ init-outline.js
@@ -4,8 +4,8 @@ export default {
   mod1: 'Course intro, project basics',
   mod2: 'Project and application development skills',
   mod3: 'Project and application development skills',
-  mod4: 'Project and application development skills',
-  mod5: 'Hackathon 1',
-  mod6: 'Hackathon 2',
+  mod4: 'Hackathon 1',
+  mod5: 'Hackathon 2',
+  mod6: 'Hackathon 3',
   mod7: 'Final and lessons learned',
 };
